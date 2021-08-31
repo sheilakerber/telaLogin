0c5fb9829f61099003927b6382265bb36a504f8e
Sheila Kerber <sheila.skerber@gmail.com>
2021-08-31 15:57:19 -0300

    primeiro commit

---
+++ telaLogin/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblUsuario = New System.Windows.Forms.Label()
+        Me.lblSenha = New System.Windows.Forms.Label()
+        Me.txtUsuario = New System.Windows.Forms.TextBox()
+        Me.txtSenha = New System.Windows.Forms.TextBox()
+        Me.btnEntrar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblUsuario
+        '
+        Me.lblUsuario.AutoSize = True
+        Me.lblUsuario.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUsuario.Location = New System.Drawing.Point(57, 59)
+        Me.lblUsuario.Name = "lblUsuario"
+        Me.lblUsuario.Size = New System.Drawing.Size(58, 16)
+        Me.lblUsuario.TabIndex = 0
+        Me.lblUsuario.Text = "Usuário:"
+        '
+        'lblSenha
+        '
+        Me.lblSenha.AutoSize = True
+        Me.lblSenha.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSenha.Location = New System.Drawing.Point(59, 101)
+        Me.lblSenha.Name = "lblSenha"
+        Me.lblSenha.Size = New System.Drawing.Size(50, 16)
+        Me.lblSenha.TabIndex = 1
+        Me.lblSenha.Text = "Senha:"
+        '
+        'txtUsuario
+        '
+        Me.txtUsuario.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUsuario.Location = New System.Drawing.Point(121, 53)
+        Me.txtUsuario.Name = "txtUsuario"
+        Me.txtUsuario.Size = New System.Drawing.Size(124, 22)
+        Me.txtUsuario.TabIndex = 2
+        '
+        'txtSenha
+        '
+        Me.txtSenha.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSenha.Location = New System.Drawing.Point(121, 95)
+        Me.txtSenha.Name = "txtSenha"
+        Me.txtSenha.Size = New System.Drawing.Size(124, 22)
+        Me.txtSenha.TabIndex = 3
+        Me.txtSenha.UseSystemPasswordChar = True
+        '
+        'btnEntrar
+        '
+        Me.btnEntrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEntrar.Location = New System.Drawing.Point(62, 154)
+        Me.btnEntrar.Name = "btnEntrar"
+        Me.btnEntrar.Size = New System.Drawing.Size(82, 23)
+        Me.btnEntrar.TabIndex = 4
+        Me.btnEntrar.Text = "Entrar"
+        Me.btnEntrar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancelar.Location = New System.Drawing.Point(158, 154)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(87, 23)
+        Me.btnCancelar.TabIndex = 5
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(304, 221)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnEntrar)
+        Me.Controls.Add(Me.txtSenha)
+        Me.Controls.Add(Me.txtUsuario)
+        Me.Controls.Add(Me.lblSenha)
+        Me.Controls.Add(Me.lblUsuario)
+        Me.Name = "Form1"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblUsuario As System.Windows.Forms.Label
+    Friend WithEvents lblSenha As System.Windows.Forms.Label
+    Friend WithEvents txtUsuario As System.Windows.Forms.TextBox
+    Friend WithEvents txtSenha As System.Windows.Forms.TextBox
+    Friend WithEvents btnEntrar As System.Windows.Forms.Button
+    Friend WithEvents btnCancelar As System.Windows.Forms.Button
+
+End Class
---
+++ telaLogin/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub btnEntrar_Click(sender As Object, e As EventArgs) Handles btnEntrar.Click
+
+        If (txtUsuario.Text = Nothing Or txtSenha.Text = Nothing) Then
+            MsgBox("O campo Usuário e Senha devem estar preenchidos.", MsgBoxStyle.Exclamation, "Atenção!")
+            Exit Sub
+        End If
+
+        If txtUsuario.Text = "ADMIN" And txtSenha.Text = "ABC123" Then
+            MsgBox("Login efetuado com sucesso!", MsgBoxStyle.OkOnly, "Logado!")
+        Else
+            MsgBox("Usuário ou senha incorreto. Por favor, verifique seus dados!", MsgBoxStyle.Critical, "Erro ao logar!")
+        End If
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        txtUsuario.Text = Nothing
+        txtSenha.Text = Nothing
+    End Sub
+End Class
